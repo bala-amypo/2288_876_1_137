db1607050248b091798fa95cf84c0eae5a2f04a4
bala-amypo <bala@amypo.in>
2025-12-18 09:37:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -3,8 +3,13 @@ package com.example.demo.controller;
 @RequestMapping("/api/users")
 public class UserAccountController{
     @AutoWired 
+    UserAccountService uas;
     @PostMapping("/api/users/pot")
     public UserAccount createuser(@RequestBody UserAccount ua){
-
+        return uas.createuser(ua);
     }
+    @GetMapping("/api/users/gt")
+    public list<
+
+    public optional<UserAccount>updateuser
 }
