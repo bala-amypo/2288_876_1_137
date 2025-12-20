1ef011e61fde734391d41819200431c853ae89c2
bala-amypo <bala@amypo.in>
2025-12-20 10:24:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/Permission.java
+++ src/main/java/com/example/demo/entity/Permission.java
@@ -1,1 +1,17 @@
-package com.explain.demo.entity;
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class {
+    @Id
+    private long id;
+    @Column(unique=true)
+    private String roleName;
+    private String description;
+    private Boolean active;
+}
