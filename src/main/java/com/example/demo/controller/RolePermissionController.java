823dbe9ce15d9639da3718b4411e64c725e35c73
bala-amypo <bala@amypo.in>
2025-12-20 14:47:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/RolePermissionController.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.RolePermission;
+
+@Repository
+public interface RolePermissionRepository extends JpaRepository<RolePermission, Long> {
+
+    List<RolePermission> findByRole_Id(Long roleId);
+}
