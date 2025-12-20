71c0da1793035645602010cc41bdaf4da0277794
bala-amypo <bala@amypo.in>
2025-12-20 13:53:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/RolePermission.java
+++ src/main/java/com/example/demo/entity/RolePermission.java
@@ -1,1 +1,17 @@
-package com.explain.demo.entity;
+package com.example.demo.entity;
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Role {
+
+    @Id
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String roleName;
+
+    private String description;
+    private Boolean active = true;
+}
