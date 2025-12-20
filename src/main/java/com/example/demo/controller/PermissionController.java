f5004e43a6f3270d9958ae5d82aa78985199ffe5
bala-amypo <bala@amypo.in>
2025-12-20 10:33:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/PermissionController.java
+++ src/main/java/com/example/demo/controller/PermissionController.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.entity.UserAccount;
-import com.example.demo.service.UserAccountService;
+import com.example.demo.entity.Permission;
+import com.example.demo.service.PermissionService;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/permissions")
 public class UserAccountController {
 
     @Autowired
