d38d33b64df1aba367dbb580d6ba5fee5861dd24
bala-amypo <bala@amypo.in>
2025-12-20 10:30:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/PermissionController.java
+++ src/main/java/com/example/demo/controller/PermissionController.java
@@ -1,1 +1,44 @@
 package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.UserAccount;
+import com.example.demo.service.UserAccountService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserAccountController {
+
+    @Autowired
+    private UserAccountService uas;
+
+    @PostMapping
+    public UserAccount createUser(@RequestBody UserAccount ua) {
+        return uas.createUser(ua);
+    }
+
+    @GetMapping
+    public List<UserAccount> getAllUsers() {
+        return uas.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserAccount getUserById(@PathVariable long id) {
+        return uas.getUserById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @PutMapping("/{id}")
+    public UserAccount updateUser(@PathVariable long id, @RequestBody UserAccount ua) {
+        return uas.updateUser(id, ua);
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public String deactivateUser(@PathVariable long id) {
+        uas.deactivateUser(id);
+        return "User deactivated successfully";
+    }
+}
--- src/main/java/com/example/demo/entity/Permission.java
+++ src/main/java/com/example/demo/entity/Permission.java
@@ -14,5 +14,37 @@ public class Permission{
     private String permissionKey;
     private String description;
     private Boolean active;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getPermissionKey() {
+        return permissionKey;
+    }
+    public void setPermissionKey(String permissionKey) {
+        this.permissionKey = permissionKey;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public Permission(long id, String permissionKey, String description, Boolean active) {
+        this.id = id;
+        this.permissionKey = permissionKey;
+        this.description = description;
+        this.active = active;
+    }
+    public Permission() {
+    }
     
 }
