45d5dee5a70a3b43d607da3401ea308cd9df7b3b
bala-amypo <bala@amypo.in>
2025-12-20 08:43:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,4 +1,4 @@
-package com.explain.demo.entity;
+package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 import jakarta.persistence.Column;
@@ -10,9 +10,40 @@ import jakarta.persistence.Id;
 public class Role{
     @Id
     private long id;
-    @column(unique=true)
+    @Column(unique=true)
     private String roleName;
     private String description;
     private Boolean active;
-    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getRoleName() {
+        return roleName;
+    }
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public Role(long id, String roleName, String description, Boolean active) {
+        this.id = id;
+        this.roleName = roleName;
+        this.description = description;
+        this.active = active;
+    }
+    public Role() {
+    }
 }
