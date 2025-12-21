23aa4513b8847117bf9c4cc2ecbffe2b471f3af2
bala-amypo <bala@amypo.in>
2025-12-21 08:50:00 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequestDto.java
+++ src/main/java/com/example/demo/dto/AuthRequestDto.java
@@ -5,16 +5,22 @@ public class AuthRequestDto {
     private String email;
     private String password;
 
-    public AuthRequestDto() {}
-
-    public AuthRequestDto(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public AuthRequestDto() {
     }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/example/demo/dto/RegisterRequestDto.java
+++ src/main/java/com/example/demo/dto/RegisterRequestDto.java
@@ -1,45 +1,35 @@
-
-// package com.example.demo.web.dto;
-
-// public class RegisterRequestDto {
-//     private String email;
-//     private String fullName;
-//     private String password;
-
-//     public RegisterRequestDto() {}
-
-//     public String getEmail() { return email; }
-//     public void setEmail(String email) { this.email = email; }
-
-//     public String getFullName() { return fullName; }
-//     public void setFullName(String fullName) { this.fullName = fullName; }
-
-//     public String getPassword() { return password; }
-//     public void setPassword(String password) { this.password = password; }
-// }
-
-package com.example.demo.web.dto;
+package com.example.demo.dto;
 
 public class RegisterRequestDto {
 
-    private String email;
     private String fullName;
+    private String email;
     private String password;
 
-    public RegisterRequestDto() {}
-
-    public RegisterRequestDto(String email, String fullName, String password) {
-        this.email = email;
-        this.fullName = fullName;
-        this.password = password;
+    public RegisterRequestDto() {
     }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getFullName() {
+        return fullName;
+    }
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
