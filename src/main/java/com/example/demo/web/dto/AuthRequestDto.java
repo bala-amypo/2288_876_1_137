aa48bfc5ea5806318fc51aab689534cff957ded7
bala-amypo <bala@amypo.in>
2025-12-21 08:33:39 +0000

    User commit

--- src/main/java/com/example/demo/web/dto/AuthRequestDto.java
+++ src/main/java/com/example/demo/web/dto/AuthRequestDto.java
@@ -1,20 +1,20 @@
-// package com.example.demo.web.dto;
+package com.example.demo.web.dto;
 
-// public class AuthRequestDto {
+public class AuthRequestDto {
 
-//     private String email;
-//     private String password;
+    private String email;
+    private String password;
 
-//     public AuthRequestDto() {}
+    public AuthRequestDto() {}
 
-//     public AuthRequestDto(String email, String password) {
-//         this.email = email;
-//         this.password = password;
-//     }
+    public AuthRequestDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
-//     public String getEmail() { return email; }
-//     public void setEmail(String email) { this.email = email; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-//     public String getPassword() { return password; }
-//     public void setPassword(String password) { this.password = password; }
-// }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
