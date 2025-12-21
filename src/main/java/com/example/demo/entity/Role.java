45df4f9c73b2edd0db76b7b67bb7c80fbf2ef237
bala-amypo <bala@amypo.in>
2025-12-21 17:56:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,3 +1,60 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Role {
+   @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id; 
+    @Column(unique = true)
+    private String roleName;
+    private String description;
+    private Boolean active;
+    
+    public Role(Long id, String roleName, String description, Boolean active) {
+        this.id = id;
+        this.roleName = roleName;
+        this.description = description;
+        this.active = active;
+    }
+    public Role() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRoleName() {
+        return roleName;
+    }
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
+
+
+
+
+
+
 // package com.example.demo.entity;
 
 // import jakarta.persistence.*;
