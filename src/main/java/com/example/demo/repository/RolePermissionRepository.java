e50130109292dc9ec1d6c891a085015bffddd4c6
bala-amypo <bala@amypo.in>
2025-12-21 18:02:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/RolePermissionRepository.java
+++ src/main/java/com/example/demo/repository/RolePermissionRepository.java
@@ -1,3 +1,22 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.example.demo.entity.RolePermission;
+
+
+
+public interface RolePermissionRepository extends JpaRepository<RolePermission, Long> {
+    
+}
+
+
+
+
+
+
+
 // package com.example.demo.repository;
 
 // import java.util.List;
