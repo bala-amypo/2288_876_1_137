1d9a6f3c07312ff22d8847750327e9ef95ccadf3
bala-amypo <bala@amypo.in>
2025-12-22 08:31:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/PermissionRepository.java
+++ src/main/java/com/example/demo/repository/PermissionRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import com.example.demo.entity.Permission;
 
-
+@Repository
 
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
     
@@ -18,8 +19,8 @@ public interface PermissionRepository extends JpaRepository<Permission, Long> {
 // package com.example.demo.repository;
 
 // import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
 // import com.example.demo.entity.Permission;
+// import org.springframework.stereotype.Repository;
 
 // import java.util.*;
 
