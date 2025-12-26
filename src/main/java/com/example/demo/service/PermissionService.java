97fd8300323670e32ba43750c3c87a117b8e26e7
bala-amypo <bala@amypo.in>
2025-12-26 04:40:08 +0000

    User commit

--- src/main/java/com/example/demo/service/PermissionService.java
+++ src/main/java/com/example/demo/service/PermissionService.java
@@ -18,24 +18,24 @@
 
 
 
+// package com.example.demo.service;
+
+// import java.util.List;
+// import com.example.demo.entity.Permission;
+
+// public interface PermissionService {
+
+//     Permission createPermission(Permission permission);
+
+//     Permission updatePermission(Long id, Permission permission);
+
+//     Permission getPermissionById(Long id);
+
+//     List<Permission> getAllPermissions();
+
+//     Permission deactivatePermission(Long id);
+// }
 package com.example.demo.service;
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.entity.Permission;
-
-public interface PermissionService {
-
-    Permission createPermission(Permission permission);
-
-    Permission updatePermission(Long id, Permission permission);
-
-    Permission getPermissionById(Long id);
-
-    List<Permission> getAllPermissions();
-
-    Permission deactivatePermission(Long id);
-}
 
 import java.util.List;
 import com.example.demo.entity.Permission;
