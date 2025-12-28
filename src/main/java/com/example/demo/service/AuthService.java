5b3a0c34d8eec7da6741d9730bb655f400b780d7
bala-amypo <bala@amypo.in>
2025-12-28 07:30:48 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -13,24 +13,24 @@ public interface AuthService {
 
 
 
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import com.example.demo.dto.AuthRequestDto;
-import com.example.demo.dto.AuthResponseDto;
-import com.example.demo.dto.RegisterRequestDto;
+// import com.example.demo.dto.AuthRequestDto;
+// import com.example.demo.dto.AuthResponseDto;
+// import com.example.demo.dto.RegisterRequestDto;
 
-public interface AuthService {
+// public interface AuthService {
 
-    /**
-     * Login user and return JWT token
-     * Used in test: t56_authService_loginSuccessWithFixedPassword
-     */
-    AuthResponseDto login(AuthRequestDto request);
+//     /**
+//      * Login user and return JWT token
+//      * Used in test: t56_authService_loginSuccessWithFixedPassword
+//      */
+//     AuthResponseDto login(AuthRequestDto request);
 
-    /**
-     * Register user
-     * Used in test: t57_authService_registerDuplicateEmailFails
-     * Must return void
-     */
-    void register(RegisterRequestDto request);
-}
+//     /**
+//      * Register user
+//      * Used in test: t57_authService_registerDuplicateEmailFails
+//      * Must return void
+//      */
+//     void register(RegisterRequestDto request);
+// }
